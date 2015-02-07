24b275e8606cf251ab7d1b6a3124b914324338a6
Jeremy Satterfield <jsatt22@gmail.com>
2015-02-07 15:08:54 -0600

    update bootstrap, ckeditor and fontawesome to latest versions

--- customizations/static/js/ckeditor/build-config.js
+++ customizations/static/js/ckeditor/build-config.js
@@ -1,5 +1,5 @@
 ﻿/**
- * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/5e8a01f49cb5fb939e57055ae5811222
+ * (2) http://ckeditor.com/builder/bd590e9f596c54bfea3a1feca421d708
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/5e8a01f49cb5fb939e57055ae5811222
+ * (3) http://ckeditor.com/builder/download/bd590e9f596c54bfea3a1feca421d708
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -34,9 +34,10 @@ var CKBUILDER_CONFIG = {
 		'bender-out.log',
 		'dev',
 		'.DS_Store',
+		'.editorconfig',
 		'.gitattributes',
 		'.gitignore',
-		'Gruntfile.js',
+		'gruntfile.js',
 		'.idea',
 		'.jscsrc',
 		'.jshintignore',
@@ -49,6 +50,8 @@ var CKBUILDER_CONFIG = {
 	],
 	plugins : {
 		'a11yhelp' : 1,
+		'autogrow' : 1,
+		'autosave' : 1,
 		'basicstyles' : 1,
 		'blockquote' : 1,
 		'clipboard' : 1,
@@ -65,13 +68,16 @@ var CKBUILDER_CONFIG = {
 		'horizontalrule' : 1,
 		'htmlwriter' : 1,
 		'image' : 1,
+		'image2' : 1,
 		'imagebrowser' : 1,
 		'indentlist' : 1,
+		'justify' : 1,
 		'link' : 1,
 		'list' : 1,
 		'magicline' : 1,
 		'mathjax' : 1,
 		'maximize' : 1,
+		'nbsp' : 1,
 		'pastefromword' : 1,
 		'pastetext' : 1,
 		'preview' : 1,
