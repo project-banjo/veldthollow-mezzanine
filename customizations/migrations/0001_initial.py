7417f65679c0f5c07136b11a4619fea30c594f27
Jeremy Satterfield <jsatt22@gmail.com>
2015-08-21 00:28:18 -0500

    fix migration dependency issue

--- customizations/migrations/0001_initial.py
+++ customizations/migrations/0001_initial.py
@@ -13,7 +13,6 @@ import customizations.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('blog', '0001_initial'),
         ('auth', '0006_require_contenttypes_0002'),
     ]
 
@@ -53,14 +52,6 @@ class Migration(migrations.Migration):
                 ('link_type', models.CharField(max_length=25, choices=[('facebook', 'Facebook'), ('twitter', 'Twitter'), ('instagram', 'Instagram'), ('web', 'Web'), ('pinterest', 'Pinterest')])),
                 ('url', models.URLField()),
                 ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='Homepage',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('featured_author', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
-                ('featured_category', models.ForeignKey(blank=True, to='blog.BlogCategory', null=True)),
             ],
         ),
     ]
