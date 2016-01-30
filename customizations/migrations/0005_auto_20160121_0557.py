01a62835a1fc04ec448a7865e7af8b9faf970b1e
Jeremy Satterfield <jsatt22@gmail.com>
2016-01-30 13:55:11 -0600

    add missing migration

---
+++ customizations/migrations/0005_auto_20160121_0557.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('customizations', '0004_populate_initial_pages'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='user',
+            options={'ordering': ('first_name', 'last_name')},
+        ),
+        migrations.AlterField(
+            model_name='user',
+            name='author_status',
+            field=models.IntegerField(default=None, verbose_name='Author Status', null=True, choices=[(None, 'No'), (5, 'Guest'), (10, 'Staff')]),
+        ),
+    ]
